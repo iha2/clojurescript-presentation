f0868a4dd00ee7dc803831fa3a761fdb27c6c382
Iha2 <iheatudeveloper@gmail.com>
2018-09-30 15:36:36 -0400

    finalizing initial setup

--- src/clojurescript_workshop/core.cljs
+++ src/clojurescript_workshop/core.cljs
@@ -1,6 +1,6 @@
-(ns starter.browser (:require
-                     ["spectacle" :as spectacle :refer [Deck, Slide, Text]]
-                     [reagent.core :as r :refer [adapt-react-class]]))
+(ns clojurescript_workshop.core (:require
+                                 ["spectacle" :as spectacle :refer [Deck, Slide, Text]]
+                                 [reagent.core :as r :refer [adapt-react-class]]))
 
 (defonce greeting "hello")
 (def who "world")
@@ -18,3 +18,4 @@
 
 (defn ^:export init []
   (start))
+(reduce #(+ %2 %1) [1 2 3 1 2 34 4 5])
